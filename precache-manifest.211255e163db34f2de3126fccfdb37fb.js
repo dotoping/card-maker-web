e48a8aa2b9f992f4f5b895d7db13af2d13853235
Daniel <coding.dotping@gmail.com>
2021-07-09 17:49:14 +1000

    Updates

---
+++ precache-manifest.211255e163db34f2de3126fccfdb37fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da68c606b5aaaeedf5f8c3c63b95b20b",
+    "url": "/card-maker-web/index.html"
+  },
+  {
+    "revision": "6aca8f935854613fe43a",
+    "url": "/card-maker-web/static/css/main.c8ca60a8.chunk.css"
+  },
+  {
+    "revision": "d9552b762b325036e2cd",
+    "url": "/card-maker-web/static/js/2.361d4aa9.chunk.js"
+  },
+  {
+    "revision": "7d2282234cd0247bd4712fb2834fc8d9",
+    "url": "/card-maker-web/static/js/2.361d4aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aca8f935854613fe43a",
+    "url": "/card-maker-web/static/js/main.74098632.chunk.js"
+  },
+  {
+    "revision": "19d3a823c8967eb2297b",
+    "url": "/card-maker-web/static/js/runtime-main.cec7c2bd.js"
+  }
+]);
